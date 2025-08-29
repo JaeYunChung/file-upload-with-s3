295ac3baf2afa858b92e138c43ddb40ab4f15e67
backend-jaeyun <chung.jy@thezooom.kr>
2025-08-29 16:23:22 +0900

    s3 이미지 올리기

--- src/main/java/file_upload_project/demo/controller/FileUploadController.java
+++ src/main/java/file_upload_project/demo/controller/FileUploadController.java
@@ -4,27 +4,34 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController("upload")
+@RestController("multipart-file")
+@RequiredArgsConstructor
 public class FileUploadController {
-    @PostMapping("multipart-file")
-    public void uploadMultifile(
+
+    private final AmazonS3Client amazonS3Client;
+
+    @PostMapping("upload")
+    public String uploadMultifile(
         @RequestPart MultipartFile file
     ) throws IOException {
+        String originalFilename = file.getOriginalFilename();
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());
         PutObjectRequest putObjectRequest = new PutObjectRequest(
-            "image-bucket",
-            "key",
+            "image-bucket-v2",
+            originalFilename,
             file.getInputStream(),
             metadata
         );
-        AmazonS3Client amazonS3Client = new AmazonS3Client();
         amazonS3Client.putObject(putObjectRequest);
+        return amazonS3Client.getUrl("image-bucket", originalFilename).toString();
     }
 }
